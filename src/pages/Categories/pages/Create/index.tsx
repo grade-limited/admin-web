53c2b48b47e256c30858f8958c5337c60be62d0e
Asatter833 <asatter833@gmail.com>
2023-11-28 22:35:05 +0600

    category create complete.

--- src/pages/Categories/pages/Create/index.tsx
+++ src/pages/Categories/pages/Create/index.tsx
@@ -6,25 +6,25 @@ import Label from "@components/Label";
 import { Cascader, Divider, Input } from "antd";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
-import { useCreateProduct } from "@/queries/products";
-import useBrand from "@/hooks/useBrand";
+import { useCreateCategories } from "@/queries/categories";
+// import useBrand from "@/hooks/useBrand";
 import Iconify from "@components/iconify";
 import useCategory from "@/hooks/useCategory";
 
 const Create: React.FC = () => {
-  const { brand, isBrandLoading, searchBrand } = useBrand();
+  // const { brand, isBrandLoading, searchBrand } = useBrand();
   const { category, isCategoryLoading, searchCategory } = useCategory();
   const { handleSubmit, control, reset } = useForm({
     // resolver: joiResolver(loginResolver),
   });
-  const { mutateAsync: create, isLoading: productCreating } =
-    useCreateProduct();
+  const { mutateAsync: create, isLoading: categoryCreating } =
+    useCreateCategories();
 
   // On Submit Function
   const onSubmit = async (data: any) => {
     message.open({
       type: "loading",
-      content: "Creating Product..",
+      content: "Creating Category..",
       duration: 0,
     });
     const res = await handleResponse(
@@ -46,13 +46,13 @@ const Create: React.FC = () => {
   return (
     <div>
       <div className="max-w-md mt-6 mx-auto text-center">
-        <p className="text-lg font-medium mb-2">Create New Product</p>
+        <p className="text-lg font-medium mb-2">Create New Category</p>
 
         <Link
-          to={"/app/products/list"}
+          to={"/app/categories/list"}
           className="text-sm font-medium text-text underline"
         >
-          <p className="mt-3">View All Products</p>
+          <p className="mt-3">View All Categories</p>
         </Link>
         <Divider />
       </div>
@@ -60,10 +60,10 @@ const Create: React.FC = () => {
         onSubmit={handleSubmit(onSubmit)}
         className="max-w-xl mb-4 mx-auto flex flex-col gap-2"
       >
-        <p className="font-medium mb-2">Product Information</p>
+        <p className="font-medium mb-2">Category Information</p>
         <div className="border p-3 rounded-md bg-slate-50">
           <div>
-            <Label className="my-1">Product Name</Label>
+            <Label className="my-1">Category Name</Label>
             <Controller
               control={control}
               name={"name"}
@@ -73,7 +73,7 @@ const Create: React.FC = () => {
                 fieldState: { error },
               }) => (
                 <Input
-                  placeholder={"Enter Product Name"}
+                  placeholder={"Enter Category Name"}
                   size={"large"}
                   onChange={onChange}
                   onBlur={onBlur}
@@ -107,10 +107,10 @@ const Create: React.FC = () => {
             />
           </div>
           <div>
-            <Label className="my-1">Brand</Label>
+            <Label className="my-1">Parent Category</Label>
             <Controller
               control={control}
-              name={"brand_id"}
+              name={"parent_id"}
               render={({
                 field: { onChange, onBlur, value },
                 fieldState: { error },
@@ -120,32 +120,7 @@ const Create: React.FC = () => {
                   size="large"
                   showSearch
                   className="w-full"
-                  placeholder={"Select a Brand..."}
-                  suffixIcon={<Iconify icon={"mingcute:search-3-line"} />}
-                  onChange={onChange}
-                  options={brand}
-                  onSearch={searchBrand}
-                  loading={isBrandLoading}
-                  status={error ? "error" : ""}
-                />
-              )}
-            />
-          </div>
-          <div>
-            <Label className="my-1">Category</Label>
-            <Controller
-              control={control}
-              name={"category_id"}
-              render={({
-                field: { onChange, onBlur, value },
-                fieldState: { error },
-              }) => (
-                <Cascader
-                  value={value}
-                  size="large"
-                  showSearch
-                  className="w-full"
-                  placeholder={"Select a Category..."}
+                  placeholder={"Select a Parent Category..."}
                   suffixIcon={<Iconify icon={"mingcute:search-3-line"} />}
                   onChange={onChange}
                   options={category}
@@ -162,7 +137,7 @@ const Create: React.FC = () => {
           size="large"
           type={"submit"}
           className="w-full mt-4"
-          disabled={productCreating}
+          disabled={categoryCreating}
         >
           Submit
         </Button>
