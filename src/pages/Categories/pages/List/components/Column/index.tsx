2f03c9c9952d438d82b5a033680e05ea775a7230
tam11a <ibrahimsadiktamim@gmail.com>
2024-01-09 06:00:56 +0600

    added: conditional column

--- src/pages/Categories/pages/List/components/Column/index.tsx
+++ src/pages/Categories/pages/List/components/Column/index.tsx
@@ -1,3 +1,4 @@
+import useQueryContext from "@/hooks/useQueryContext";
 import { useDeleteCategories } from "@/queries/categories";
 import { ICategoryId } from "@/types";
 import handleResponse from "@/utilities/handleResponse";
@@ -10,207 +11,218 @@ import moment from "moment";
 import { Link } from "react-router-dom";
 
 const Column = (): GridColDef[] => {
-  const { mutateAsync: Delete, isLoading: isDeleteLoading } =
-    useDeleteCategories();
-  const onDelete = async (
-    id: ICategoryId,
-    permanent: any = null,
-    restore: any = null
-  ) => {
-    message.open({
-      type: "loading",
-      content: permanent
-        ? "Deleting Category Permanently.."
-        : restore
-        ? "Restoring Category.."
-        : "Deleting Category..",
-      duration: 0,
-    });
-    const res = await handleResponse(() =>
-      Delete({
-        id,
-        params: {
-          permanent,
-          restore,
-        },
-      })
-    );
+	const { mutateAsync: Delete, isLoading: isDeleteLoading } =
+		useDeleteCategories();
+	const onDelete = async (
+		id: ICategoryId,
+		permanent: any = null,
+		restore: any = null
+	) => {
+		message.open({
+			type: "loading",
+			content: permanent
+				? "Deleting Category Permanently.."
+				: restore
+				? "Restoring Category.."
+				: "Deleting Category..",
+			duration: 0,
+		});
+		const res = await handleResponse(() =>
+			Delete({
+				id,
+				params: {
+					permanent,
+					restore,
+				},
+			})
+		);
 
-    message.destroy();
+		message.destroy();
 
-    if (res.status) {
-      message.success(res.message);
-      return true;
-    } else {
-      message.error(res.message);
-      return false;
-    }
-  };
+		if (res.status) {
+			message.success(res.message);
+			return true;
+		} else {
+			message.error(res.message);
+			return false;
+		}
+	};
 
-  // const { mutateAsync: Suspend, isLoading: isSuspendLoading } =
-  //   useSuspendEmployee();
+	// const { mutateAsync: Suspend, isLoading: isSuspendLoading } =
+	//   useSuspendEmployee();
 
-  // const onSuspend = async (id: ICategoriesId) => {
-  //   message.open({
-  //     type: "loading",
-  //     content: "Suspending Employee..",
-  //     duration: 0,
-  //   });
-  //   const res = await handleResponse(() =>
-  //     Suspend({
-  //       id,
-  //     })
-  //   );
+	// const onSuspend = async (id: ICategoriesId) => {
+	//   message.open({
+	//     type: "loading",
+	//     content: "Suspending Employee..",
+	//     duration: 0,
+	//   });
+	//   const res = await handleResponse(() =>
+	//     Suspend({
+	//       id,
+	//     })
+	//   );
 
-  //   message.destroy();
+	//   message.destroy();
 
-  //   if (res.status) {
-  //     message.success(res.message);
-  //     return true;
-  //   } else {
-  //     message.error(res.message);
-  //     return false;
-  //   }
-  // };
+	//   if (res.status) {
+	//     message.success(res.message);
+	//     return true;
+	//   } else {
+	//     message.error(res.message);
+	//     return false;
+	//   }
+	// };
+	const { watch } = useQueryContext();
 
-  return [
-    {
-      headerName: "ID",
-      headerAlign: "center",
-      field: "id",
-      align: "center",
-      flex: 1,
-      filterable: false,
-      sortable: false,
-    },
-    {
-      headerName: "Title",
-      headerAlign: "center",
-      field: "name",
-      align: "center",
-      minWidth: 200,
-      flex: 1,
-      filterable: false,
-      sortable: false,
-      // valueGetter(params) {
-      //   return (
-      //     [params?.row?.name, params?.row?.last_name].join(" ") || "-"
-      //   );
-      // },
-    },
+	return [
+		{
+			headerName: "ID",
+			headerAlign: "center",
+			field: "id",
+			align: "center",
+			flex: 1,
+			filterable: false,
+			sortable: false,
+		},
+		{
+			headerName: "Title",
+			headerAlign: "center",
+			field: "name",
+			align: "center",
+			minWidth: 200,
+			flex: 1,
+			filterable: false,
+			sortable: false,
+			// valueGetter(params) {
+			//   return (
+			//     [params?.row?.name, params?.row?.last_name].join(" ") || "-"
+			//   );
+			// },
+		},
 
-    {
-      headerName: "Parent Category",
-      headerAlign: "center",
-      field: "parent",
-      align: "center",
-      flex: 1,
-      minWidth: 250,
-      filterable: false,
-      sortable: false,
-      renderCell: (data: any) =>
-        data?.row?.parent ? (
-          <Link to={`/app/categories/i/${data?.row?.parent_id}`}>
-            <Chip label={data?.row?.parent?.name} />
-          </Link>
-        ) : (
-          // <Tag color="default">{data?.row?.brand?.name}</Tag>
-          "-"
-        ),
-    },
-    {
-      headerName: "Created At",
-      headerAlign: "center",
-      field: "created_at",
-      align: "center",
-      flex: 1,
-      width: 200,
-      minWidth: 180,
-      filterable: false,
-      sortable: false,
-      valueFormatter(params) {
-        return moment(params.value).format("lll");
-      },
-    },
-    {
-      headerName: "Updated At",
-      headerAlign: "center",
-      field: "updated_at",
-      align: "center",
-      flex: 1,
-      width: 200,
-      minWidth: 180,
-      filterable: false,
-      sortable: false,
-      valueFormatter(params) {
-        return moment(params.value).format("lll");
-      },
-    },
-    {
-      field: "actions",
-      type: "actions",
-      flex: 1,
-      minWidth: 160,
-      getActions: (params) => [
-        <GridActionsCellItem
-          disableRipple
-          disableTouchRipple
-          disableFocusRipple
-          className="hover: bg-transparent"
-          icon={
-            <Link to={`/app/categories/i/${params.id}`}>
-              <Button type="dashed">View</Button>
-            </Link>
-          }
-          label="Details"
-        />,
-        <GridActionsCellItem
-          icon={
-            <Link to={`/app/categories/i/${params.id}/edit`}>
-              <Iconify icon={"fluent:edit-12-regular"} className="text-lg" />
-            </Link>
-          }
-          label="Edit"
-        />,
-        // <GridActionsCellItem
-        //   icon={
-        //     params.row.is_active ? (
-        //       <Iconify icon={"lucide:shield-ban"} className="text-lg" />
-        //     ) : (
-        //       <Iconify icon={"tabler:shield-up"} className="text-lg" />
-        //     )
-        //   }
-        //   disabled={isSuspendLoading}
-        //   showInMenu
-        //   label={
-        //     params.row.is_active ? "Suspend Employee" : "Activate Employee"
-        //   }
-        //   onClick={() => onSuspend(params.id)}
-        // />,
-        <GridActionsCellItem
-          icon={
-            <Iconify icon={"icon-park-twotone:delete"} className="text-lg" />
-          }
-          disabled={isDeleteLoading}
-          showInMenu
-          label="Delete"
-          onClick={() => onDelete(params.id)}
-        />,
-        <GridActionsCellItem
-          icon={
-            <Iconify
-              icon={"icon-park-twotone:delete-five"}
-              className="text-lg"
-            />
-          }
-          disabled={isDeleteLoading}
-          showInMenu
-          label="Permanently Delete"
-          onClick={() => onDelete(params.id, true)}
-        />,
-      ],
-    },
-  ];
+		...((watch("only_parent") as boolean)
+			? ([] as GridColDef[])
+			: ([
+					{
+						headerName: "Parent Category",
+						headerAlign: "center",
+						field: "parent",
+						align: "center",
+						flex: 1,
+						minWidth: 250,
+						filterable: false,
+						sortable: false,
+						renderCell: (data: any) =>
+							data?.row?.parent ? (
+								<Link to={`/app/categories/i/${data?.row?.parent_id}`}>
+									<Chip label={data?.row?.parent?.name} />
+								</Link>
+							) : (
+								// <Tag color="default">{data?.row?.brand?.name}</Tag>
+								"-"
+							),
+					},
+			  ] as GridColDef[])),
+		{
+			headerName: "Created At",
+			headerAlign: "center",
+			field: "created_at",
+			align: "center",
+			flex: 1,
+			width: 200,
+			minWidth: 180,
+			filterable: false,
+			sortable: false,
+			valueFormatter(params) {
+				return moment(params.value).format("lll");
+			},
+		},
+		{
+			headerName: "Updated At",
+			headerAlign: "center",
+			field: "updated_at",
+			align: "center",
+			flex: 1,
+			width: 200,
+			minWidth: 180,
+			filterable: false,
+			sortable: false,
+			valueFormatter(params) {
+				return moment(params.value).format("lll");
+			},
+		},
+		{
+			field: "actions",
+			type: "actions",
+			flex: 1,
+			minWidth: 160,
+			getActions: (params) => [
+				<GridActionsCellItem
+					disableRipple
+					disableTouchRipple
+					disableFocusRipple
+					className="hover: bg-transparent"
+					icon={
+						<Link to={`/app/categories/i/${params.id}`}>
+							<Button type="dashed">View</Button>
+						</Link>
+					}
+					label="Details"
+				/>,
+				<GridActionsCellItem
+					icon={
+						<Link to={`/app/categories/i/${params.id}/edit`}>
+							<Iconify
+								icon={"fluent:edit-12-regular"}
+								className="text-lg"
+							/>
+						</Link>
+					}
+					label="Edit"
+				/>,
+				// <GridActionsCellItem
+				//   icon={
+				//     params.row.is_active ? (
+				//       <Iconify icon={"lucide:shield-ban"} className="text-lg" />
+				//     ) : (
+				//       <Iconify icon={"tabler:shield-up"} className="text-lg" />
+				//     )
+				//   }
+				//   disabled={isSuspendLoading}
+				//   showInMenu
+				//   label={
+				//     params.row.is_active ? "Suspend Employee" : "Activate Employee"
+				//   }
+				//   onClick={() => onSuspend(params.id)}
+				// />,
+				<GridActionsCellItem
+					icon={
+						<Iconify
+							icon={"icon-park-twotone:delete"}
+							className="text-lg"
+						/>
+					}
+					disabled={isDeleteLoading}
+					showInMenu
+					label="Delete"
+					onClick={() => onDelete(params.id)}
+				/>,
+				<GridActionsCellItem
+					icon={
+						<Iconify
+							icon={"icon-park-twotone:delete-five"}
+							className="text-lg"
+						/>
+					}
+					disabled={isDeleteLoading}
+					showInMenu
+					label="Permanently Delete"
+					onClick={() => onDelete(params.id, true)}
+				/>,
+			],
+		},
+	];
 };
 
 export default Column;
